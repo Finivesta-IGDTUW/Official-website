d6aa1138d735c5cfdcf8543a09028d751e98c648
priyasinghh29 <143327559+priyasinghh29@users.noreply.github.com>
2024-03-25 09:30:00 +0530

    Add files via upload

---
+++ book reviews/src/index.js
@@ -0,0 +1,12 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> {/* Render main component */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
