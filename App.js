6b42124c855493831a3d5392bc352b383f728e9f
DishaSethi <142926792+DishaSethi@users.noreply.github.com>
2024-05-20 15:39:39 +0530

    Events Page Code

---
+++ App.js
@@ -0,0 +1,158 @@
+
+import './App.css';
+//  tslint:disable:no-unused-variable 
+// import img2 from './imgs/Speaker.png' ;
+
+function App() {
+  return (
+
+
+
+
+    // My code
+    <div className="App">
+ 
+     <h1 className="Heading">Events</h1>
+
+    
+    
+   <div>
+      <h1 className="Year">2024</h1>
+
+      <div className="Images">
+        <div className="flex1">
+       
+
+        <div className="element">
+          
+          <div className="element3">
+            {/*  Finlatics*/}
+            <a href='https://www.linkedin.com/feed/update/urn:li:activity:7160561756463853568/' target='_blank' rel='noreferrer'>
+            <img src={"./imgs/Finlatics.png"} alt="Finlatics"/>
+            </a>
+          </div>
+  
+          <h2 className="title">Finlatics</h2>
+  
+          </div>
+        
+
+
+        <div className="element">
+        <div className="element2">
+          {/* Money Melange */}
+          <a href='https://www.instagram.com/p/C2opSv9yFFC/' target='_blank' rel='noreferrer'>
+        <img src={"./imgs/Melange.png"} alt="Money Melange"/>
+        </a>
+        </div>
+
+        <h2 className="title">Money Melange</h2>
+
+        </div>
+      
+
+
+        </div>
+        
+        <div className="flex1">
+
+
+       
+        
+        <div className="element">
+        <div className="element1">
+          {/* Kharche pe Charcha */}
+          <a href='https://www.instagram.com/p/C2AVhH4SE0P/' target='_blank' rel='noreferrer'>
+          <img src={"./imgs/Kharche pe charcha.png"} alt="kharche pr charcha"/>
+          </a>
+        </div>
+       
+        <h2 className="title">Kharche Pe Charcha</h2>
+        </div>
+        
+
+        </div>
+        
+        </div>
+      </div>
+
+
+
+   <div>
+      <h1 className="Year">2023</h1>
+
+      <div className="Images">
+        <div className="flex1">
+       
+
+          <div className="element">
+        <div className="element1">
+          {/* bidding bitz */}
+          <a href='https://www.instagram.com/p/CzqYKwRLp24/' target='_blank' rel='noreferrer'>
+          <img src={"./imgs/BiddingBitz.png"} alt="speaker"/>
+          </a>
+        </div>
+       
+        <h2 className="title">Bidding Blitz</h2>
+        </div>
+        
+
+
+        <div className="element">
+        <div className="element2">
+          {/* speaker */}
+          <a href='https://www.instagram.com/p/CzbC7cLPrex/' target='_blank' rel='noreferrer'>
+        <img src={"./imgs/Speaker.png"} alt="speaker"/>
+        </a>
+        </div>
+
+        <h2 className="title">Speaker</h2>
+
+        </div>
+      
+
+
+        </div>
+        
+        <div className="flex1">
+
+
+        <div className="element">
+          
+        <div className="element3">
+          {/*  college trader showdown*/}
+          <a href='https://www.instagram.com/p/CxXRs1Pywdn/' target='_blank' rel='noreferrer'>
+          <img src={"./imgs/CollegeTrading.png"} alt="speaker"/>
+          </a>
+        </div>
+
+        <h2 className="title">College Trader Showdown</h2>
+
+        </div>
+        
+        
+
+        <div className="element">
+          
+        <div className="element4">
+          {/*  orientation*/}
+          <a href='https://www.instagram.com/p/CxAA9weLEea/?img_index=1' target='_blank' rel='noreferrer'>
+          <img src={"./imgs/Orientation.png"} alt="speaker"/>
+          </a>
+        </div>
+
+        <h2 className="title">Orientation</h2>
+
+        </div>
+
+        </div>
+        
+        </div>
+      </div>
+
+      </div>
+      
+  );
+}
+
+export default App;
