1757eb47f53952f276b7649379ae28ccd41b6fe2
Jhalak-Gupta <143864523+Jhalak-Gupta@users.noreply.github.com>
2024-07-25 20:02:20 +0530

    Individual Blogs added

--- finivesta/src/Resources/Blogs/Blog.js
+++ finivesta/src/Resources/Blogs/Blog.js
@@ -10,111 +10,81 @@ import I6 from '../Images/blogs/6.jpg';
 import I7 from '../Images/blogs/7.jpg';
 
 const Blog = () => {
-
-    const redirectToLink = (url) => {
-        window.location.href = url;
-    };
-
-  return (
+    return (
         <div>
-
-            <div class="header">
+            <div classNameName="header">
                 <h1>Blogs</h1>
-                <p>Access the wealth of knowledge and tools in our <br/>resource library.</p>
+                <p>Access the wealth of knowledge and tools in our <br/> resource library.</p>
             </div>
-
-
-            <div class="resourcesbox">
-
-            <div class="boxsmall blog" >
-            {/* onClick={() => redirectToLink("https://docs.google.com/document/d/1JPCbYWxeHt82bj_XBnxiC_ILS0an8TfEgUt6rhlG-hk/edit?usp=sharing")} */}
-                <a href="/resources/blogs/AI-in-finance">
-                <div class="img">
-                    <img src={I1} alt="Cover"/>
+            <div className="resourcesbox">
+                <div className="boxsmall blog" >
+                    <a href="/resources/blogs/AI-in-finance">
+                    <div className="img">
+                        <img src={I1} alt="Cover"/>
+                    </div>
+                    <div className="text">
+                        <h2>AI in Finance</h2>
+                    </div>
+                    </a>
                 </div>
-                <div class="text">
-                    <h2>AI in Finance</h2>
-                    <a>The financial industry and AI</a>
-                </div>
-                </a>
-            </div>
 
-            <div class="boxsmall blog" >
-            {/* onClick={() => redirectToLink("https://docs.google.com/document/d/14UnacR779oQ6MPQMj1Lw1OAdCSSSHjzmtb8LXzM5b1A/edit?usp=sharing")}> */}
-                <a href="/resources/blogs/finance-and-technology">
-                <div class="img">
-                <img src={I2} alt="Cover"/>
+                <div className="boxsmall blog" >
+                    <a href="/resources/blogs/finance-and-technology">
+                    <div className="img">
+                         <img src={I2} alt="Cover"/>
+                    </div>
+                    <div className="text">
+                       <h2>Finance and Technology</h2>          
+                    </div>
+                    </a>
                 </div>
-                <div class="text">
-                <h2>Finance and Technology</h2>
-                <a>All about fintech</a>          
-            </div>
-                </a>
-            </div>
 
-            <div class="boxsmall blog" onClick={() => redirectToLink("")}>
-                <a href="/resources/blogs/power-of-compounding">
-                <div class="img">
-                <img src={I3} alt="Cover"/>
+                <div className="boxsmall blog">
+                    <a href="/resources/blogs/power-of-compounding">
+                    <div className="img">
+                      <img src={I3} alt="Cover"/>
+                    </div>
+                    <div className="text">
+                        <h2>Power of Compounding</h2>
+                    </div>
+                    </a>
                 </div>
-                <div class="text">
-                <h2>Power of Compounding</h2>
-                </div>
-                </a>
-            </div>
             
-            <div class="boxsmall blog" onClick={() => redirectToLink("")}>
-                <a href="/resources/blogs/liquidation">
-                <div class="img">
-                <img src={I4} alt="Cover"/>
+                <div className="boxsmall blog">
+                    <a href="/resources/blogs/liquidation">
+                    <div className="img">
+                        <img src={I4} alt="Cover"/>
+                    </div>
+                    <div className="text">
+                        <h2>Liquidation</h2>
+                    </div>
+                    </a>
                 </div>
-                <div class="text">
-                <h2>Liquidation</h2>
-            </div>
-                </a>
-            </div>
 
-            <div class="boxsmall blog"> 
-                {/* onClick={() => redirectToLink("https://docs.google.com/document/d/1YZBWhPdx8-3grhuQFB4O48oBolRoDktjeqqlCJTduVI/edit?usp=drivesdk")}> */}
-                <a href="/resources/blogs/financial-literacy">
-                <div class="img">
-                <img src={I5} alt="Cover"/>
+                <div className="boxsmall blog"> 
+                    <a href="/resources/blogs/financial-literacy">
+                    <div className="img">
+                        <img src={I5} alt="Cover"/>
+                    </div>
+                    <div className="text">
+                        <h2>Financial Literacy</h2>
+                    </div>
+                    </a>
                 </div>
-                <div class="text">
-                <h2>Financial Literacy</h2>
-                <a>Importance of learning finance</a>
-            </div>
-                </a>
-            </div>
 
-            <div class="boxsmall blog" onClick={() => redirectToLink("")}>
-                <a href="/resources/blogs/bidding">
-                <div class="img">
-                    <img src={I6} alt="Cover"/>
+                <div className="boxsmall blog">
+                    <a href="/resources/blogs/bidding">
+                    <div className="img">
+                        <img src={I6} alt="Cover"/>
+                    </div>
+                    <div className="text">
+                        <h2>Bidding</h2>
+                    </div>
+                    </a>
                 </div>
-                <div class="text">
-                    <h2>Bidding</h2>
-                </div>
-                </a>
             </div>
-
-            {/* Repeated <div class="boxsmall blog" onClick={() => redirectToLink("")}>
-                <a href="/resources/blogs">
-                <div class="img">
-                    <img src={I7} alt="Cover"/>
-                </div>
-                <div class="text">
-                    <h2>Liquidation</h2>
-                </div>
-                </a>
-            </div> */}
-
-
-            </div>
-
-        </div>
-        
-  );
-};
+        </div>         
+    );
+  };
 
 export default Blog;
