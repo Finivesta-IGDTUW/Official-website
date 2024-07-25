6d613d8f92c85b41d5c6cd42045f9a5d2a5d1eb2
Jhalak-Gupta <143864523+Jhalak-Gupta@users.noreply.github.com>
2024-07-25 17:15:29 +0530

    Individual Blogs added

--- finivesta/src/Resources/Blogs/Blogpages/power-of-compounding.js
+++ finivesta/src/Resources/Blogs/Blogpages/power-of-compounding.js
@@ -1,11 +1,34 @@
 import React from "react";
+import "./blogpage.css";
 
 const Powerofcompounding = () => {
   return (
         <div>
             <div className="blog-page-box">
-                <div class="blog-header"></div>                
-                <div className="blog-para"></div>
+                <div className="blog-header" style={{ backgroundImage: "url(comp-img.png)" }}>
+                    <h1>Power of Compounding</h1>
+                    <p> Wealth unlocks through <br/> Compounding Magic </p>
+                </div>
+                <div className="blog-para">                    
+                    <p> Compounding is the act of generating interest on both your initial investment and previous interest gained. It's like a snowball rolling down a hill, getting bigger and bigger as more snow accumulates. Compounding can be a highly strong strategy for increasing your wealth over time. </p>
+                    <p> Assume you invest Rs 1,000 in an account that earns 5% interest each year to better understand how compounding works. You will have earned Rs.50 in interest by the end of the first year. This interest is then added to your main investment, so your account will have Rs.1,050 at the start of the second year. You will earn income on both your Rs.1,000 principle investment and your Rs.50 interest from the first year in the second year. In the second year, you will earn Rs.52.50 in interest. Over the span of years, this compounding can plays a huge role in increasing our assets. </p>
+                    <p> When getting into compounding, whether for investments or managing debt, consider the following factors to maximize its benefits: </p>
+                    <ul>
+                        <li> Initial Investment Amount </li>
+                        <li> Time Horizon </li>
+                        <li> Interest Rate </li>
+                        <li> Compounding Frequency </li>
+                        <li> Reinvestment of Earnings </li>
+                        <li> Regular Contributions </li>
+                        <li> Investment Vehicles </li>
+                        <li> Risk Tolerance  </li>
+                    </ul>
+                    <p> The concept of compounding is beautifully illustrated by Albert Einstein's famous quote: “Compound interest is the eighth wonder of the world. He who understands it, earns it; he who doesn't, pays it.” Compounding works best when investments are allowed to grow over long periods, leveraging both initial capital and accumulated earnings. </p>
+                    <p> The key to harnessing the power of compounding is consistency and time. Regularly investing small amounts, even if modest, can yield impressive results due to the effect of compounding over time. Additionally, starting early maximizes the benefits of compounding. The longer your money is invested, the more it benefits from compounding. </p>
+                    <p> Additionally, compounding is not limited to traditional investments like savings accounts or stocks. It applies to virtually any area where returns can be reinvested, including reinvested dividends or compounded interest on loans. </p>
+                    <p> Compounding is often referred to as the eighth wonder of the world, and for good reason. It's a financial principle that can dramatically enhance your wealth over time, leveraging the power of interest to work in your favor. Understanding and utilizing compounding can be a game-changer for anyone looking to build long-term financial stability. </p>
+                    <p> In summary, the power of compounding is a fundamental financial principle that underscores the importance of time and patience in wealth-building. Embracing this concept can help you achieve long-term financial goals and enjoy the rewards of diligent investing. </p>
+                </div>
             </div>
         </div>
   );
