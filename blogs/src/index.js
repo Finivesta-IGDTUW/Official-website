342bc7655244938d83a95a20c72a88e03f83b8e6
urvashi2004 <uy2204@gmail.com>
2024-07-26 12:29:31 +0530

    Removed the uncompiled part

--- blogs/src/index.js
+++
@@ -1,6 +0,0 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Import your main App component
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- youtube channels/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import YoutubeChannels from './YoutubeChannels';
-
-ReactDOM.render(<YoutubeChannels />, document.getElementById('root'));
