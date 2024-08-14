b981348fed9f3eb40992ea917f7b195696da25b7
priyasinghh29 <143327559+priyasinghh29@users.noreply.github.com>
2024-08-14 20:20:40 +0530

    Update Stocks.js

--- src/Resources/StockMarket/Stocks.js
+++ src/Resources/StockMarket/Stocks.js
@@ -22,7 +22,7 @@ const Stocks = () => {
             <div className="box1">
             
                 <div className="img">
-                    <img src="How to start investing .jpeg" alt="Budgeting" />
+                    <img src={I1} alt="Cover"/>
                 </div>
                 <div className="text">
                     <h2>How to start investing?</h2>
@@ -36,7 +36,7 @@ const Stocks = () => {
         <a href="https://www.reddit.com/r/IndiaInvestments/comments/9ltgni/for_someone_who_is_absolutely_at_level_zero_in/" class="box-link">
             <div className="box1">
                 <div className="img">
-                  <img src="Investing for someone who is at level zero.jpeg" alt="Investing" />
+                  <img src={I1} alt="Cover"/>
                 </div>
                 <div className="text">
                   <h2>Investing for someone who is at level zero</h2>
@@ -49,7 +49,7 @@ const Stocks = () => {
         <a href="https://www.fool.com/investing/stock-market/types-of-stocks/" class="box-link">
             <div className="box1">
                 <div className="img">
-                  <img src="types of stocks.jpeg" alt="Stock/Share Market" />
+                  <img src={I1} alt="Cover"/>
                 </div>
                 <div className="text">
                   <h2>Types of Stocks</h2>
@@ -67,7 +67,7 @@ const Stocks = () => {
         <a href="https://www.forbes.com/advisor/investing/what-are-stocks/" class="box-link">
             <div className="box1">
                 <div className="img">
-                  <img src="what are stocks.jpg" alt="Crypto" />
+                  <img src={I1} alt="Cover"/>
                 </div>
                 <div className="text">
                   <h2>What are stocks?</h2>
@@ -80,7 +80,7 @@ const Stocks = () => {
         <a href="https://www.reddit.com/r/IndiaInvestments/comments/k7v0dg/a_beginners_guide_to_investing_in_the_stock/" class="box-link">
             <div className="box1">
                 <div className="img">
-                  <img src="Beginners guide to investing .jpeg" alt="Financial Literacy" />
+                  <img src={I1} alt="Cover"/>
                 </div>
                 <div className="text">
                   <h2>Beginners guide to investing</h2>
@@ -92,7 +92,7 @@ const Stocks = () => {
         <a href="https://www.investopedia.com/terms/s/stock.asp" class="box-link">
             <div className="box1">
                 <div className="img">
-                  <img src="stocksvsbonds.jpg" alt="Bidding" />
+                  <img src={I1} alt="Cover"/>
                 </div>
                 <div className="text">
                   <h2>Stocks VS Bonds</h2>
