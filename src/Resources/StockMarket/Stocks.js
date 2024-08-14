171a7a31fcabc0d986bfa2bf563ac9bf0db50b7f
priyasinghh29 <143327559+priyasinghh29@users.noreply.github.com>
2024-08-14 20:17:19 +0530

    Update Stocks.js

--- src/Resources/StockMarket/Stocks.js
+++ src/Resources/StockMarket/Stocks.js
@@ -1,90 +1,109 @@
 import React from "react";
 import "../Resources.css";
 
-import I1 from '../Images/blogs/1.jpg';
+import I1 from '../Images/bg1.jpg';
+import I2 from '../Images/bg1.jpg';
+import I3 from '../Images/bg1.jpg';
+import I4 from '../Images/bg1.jpg';
+import I5 from '../Images/bg1.jpg';
+import I6 from '../Images/bg1.jpg';
+import I7 from '../Images/bg1.jpg';
+
 
 const Stocks = () => {
   return (
-    <div className="blogpage">
-        <div className="header">
-            <h1>Stock Market & Trading</h1>
-            <p>Access the wealth of knowledge and tools in our <br/> resource library.</p>
-        </div>
+    <div className="header">
+        <h1>Stock Market</h1>
+        <p>Access the wealth of knowledge and tools in our <br />resource library.</p>
+    </div>
+
+    <div className="box">
+        <a href="https://fortune.com/recommends/investing/how-to-start-investing/" class="box-link">
+            <div className="box1">
+            
+                <div className="img">
+                    <img src="How to start investing .jpeg" alt="Budgeting" />
+                </div>
+                <div className="text">
+                    <h2>How to start investing?</h2>
+                
+                </div>
+            </div>
+
+        </a>
         
-        <div className="resourcesbox">
-            <div className="boxsmall blog" >
-                <a href="/resources/blogs/AI-in-finance">
-                <div className="img">
-                    <img src={I1} alt="Cover"/>
-                </div>
-                <div className="text">
-                    <h2>AI in Finance</h2>
-                    <p className="blog-byline-card"> World of artificial intelligence overlaps with that of finance</p>
-                </div>
-                </a>
-            </div>
-
-            <div className="boxsmall blog" >
-                <a href="/resources/blogs/finance-and-technology">
-                <div className="img">
-                    <img src={I1} alt="Cover"/>
-                </div>
-                <div className="text">
-                <h2>Finance and Technology</h2>   
-                <p className="blog-byline-card">Bridging Finance and Innovation</p>       
-                </div>
-                </a>
-            </div>
-
-            <div className="boxsmall blog">
-                <a href="/resources/blogs/power-of-compounding">
-                <div className="img">
-                <img src={I1} alt="Cover"/>
-                </div>
-                <div className="text">
-                    <h2>Power of Compounding</h2>
-                    <p className="blog-byline-card" >Wealth unlocks through Compounding Magic </p>
-                </div>
-                </a>
-            </div>
         
-            <div className="boxsmall blog">
-                <a href="/resources/blogs/liquidation">
+        <a href="https://www.reddit.com/r/IndiaInvestments/comments/9ltgni/for_someone_who_is_absolutely_at_level_zero_in/" class="box-link">
+            <div className="box1">
                 <div className="img">
-                    <img src={I1} alt="Cover"/>
+                  <img src="Investing for someone who is at level zero.jpeg" alt="Investing" />
                 </div>
                 <div className="text">
-                    <h2>Liquidation</h2>
-                    <p className="blog-byline-card">Turning assets into Financial Opportunities </p>
+                  <h2>Investing for someone who is at level zero</h2>
+                  
                 </div>
-                </a>
             </div>
 
-            <div className="boxsmall blog"> 
-                <a href="/resources/blogs/financial-literacy">
+        </a>
+
+        <a href="https://www.fool.com/investing/stock-market/types-of-stocks/" class="box-link">
+            <div className="box1">
                 <div className="img">
-                    <img src={I1} alt="Cover"/>
+                  <img src="types of stocks.jpeg" alt="Stock/Share Market" />
                 </div>
                 <div className="text">
-                    <h2>Financial Literacy</h2>
-                    <p className="blog-byline-card"> Empowering Education and Mastering Finance </p>
+                  <h2>Types of Stocks</h2>
+                  
                 </div>
-                </a>
             </div>
 
-            <div className="boxsmall blog">
-                <a href="/resources/blogs/bidding">
+        </a>
+        
+
+        
+    </div>
+
+    <div className="box">
+        <a href="https://www.forbes.com/advisor/investing/what-are-stocks/" class="box-link">
+            <div className="box1">
                 <div className="img">
-                    <img src={I1} alt="Cover"/>
+                  <img src="what are stocks.jpg" alt="Crypto" />
                 </div>
                 <div className="text">
-                    <h2>Bidding</h2>
-                    <p className="blog-byline-card">Strategic Bidding: Key to Success </p>
+                  <h2>What are stocks?</h2>
+                  
                 </div>
-                </a>
             </div>
-        </div>
-    </div>   
+
+        </a>
+        
+        <a href="https://www.reddit.com/r/IndiaInvestments/comments/k7v0dg/a_beginners_guide_to_investing_in_the_stock/" class="box-link">
+            <div className="box1">
+                <div className="img">
+                  <img src="Beginners guide to investing .jpeg" alt="Financial Literacy" />
+                </div>
+                <div className="text">
+                  <h2>Beginners guide to investing</h2>
+                </div>
+            </div>
+
+        </a>
+        
+        <a href="https://www.investopedia.com/terms/s/stock.asp" class="box-link">
+            <div className="box1">
+                <div className="img">
+                  <img src="stocksvsbonds.jpg" alt="Bidding" />
+                </div>
+                <div className="text">
+                  <h2>Stocks VS Bonds</h2>
+                </div>
+            </div>
+
+        </a>
+        
+    </div>
+    
+
         
   );
 };
