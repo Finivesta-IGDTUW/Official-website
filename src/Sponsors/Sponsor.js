e5fca8c7b50293b6f6ef19718b4d1ae95d099f10
Jasleen1210 <05kaur.j@gmail.com>
2024-08-15 14:59:13 +0530

    Sponsors page
    Added header image as well as intro line to the sponsors page
    Added some hover styles to the logos
    Also added styling to the buttons on home page

--- src/Sponsors/Sponsor.js
+++ src/Sponsors/Sponsor.js
@@ -8,18 +8,11 @@ import {Helmet} from 'react-helmet';
 const Sponsor = () => {
     return (
         <>
-            <Helmet>
-                <meta charset="UTF-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <link href="/file/style.css" rel="stylesheet"/>
-                <title>Finivesta</title>
-                {/* <link rel="icon" href={Fav}/> FAVICON KE LIYE BLANK SYNTAX */}
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet"/>
-            </Helmet>
+            <div className="sponsors-header">
+            <h1>Our Sponsors</h1>
+            <p>Empowering innovation and growth, <br /> thanks to the support of our valued sponsors.</p>
+            </div>
 
-            <h1 class="our_sponsors">Our Sponsors</h1>
             <div class="sponsors_container">
                 {/* Added blank links to link logo to the page of the sponsors */}
                 <div class="logos" id="a1">
