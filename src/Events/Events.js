b55cda73899a9945f45791182e845e174d3f3d1c
Disha <sethidisha123@gmail.com>
2024-09-03 12:16:26 +0530

    Hover effects on events page and center aligned the Year for responsiveness

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -9,7 +9,21 @@ import Melange from './Images/Melange.png';
 import Orientation from './Images/Orientation.png';
 import Speaker from './Images/Speaker.png';
 
-const Events = () => {
+const Events = () =>{
+  document.addEventListener('DOMContentLoaded', function() {
+    var photos = document.querySelectorAll('.images-wrapper');
+
+    photos.forEach(function(photo) {
+        photo.addEventListener('touchstart', function() {
+            photo.classList.add('touch');
+        });
+
+        photo.addEventListener('touchend', function() {
+            photo.classList.remove('touch');
+        });
+    });
+}); 
+  
   return (
     <div className="Events">
     <div className="events-header">
@@ -28,11 +42,27 @@ const Events = () => {
 
        <div className="element">
          
-         <div className="element3">
+         <div className="element3 ele">
            {/*  Finlatics*/}
+     
+
+           
+           <div class="image-wrapper">
+           <img className="image" src={Finlatics} alt="Finlatics"/>
+           <div class="overlay">
+            
            <a href='https://www.linkedin.com/feed/update/urn:li:activity:7160561756463853568/' target='_blank' rel='noreferrer'>
-           <img src={Finlatics} alt="Finlatics"/>
-           </a>
+           <i  className="fab fa-linkedin-in"/>
+           </a> 
+           </div>
+           </div>
+       
+
+                 
+            
+          
+    
+        
          </div>
  
          <h2 className="title">Finlatics Seminar</h2>
@@ -42,11 +72,16 @@ const Events = () => {
 
 
        <div className="element">
-       <div className="element2">
+       <div className="element2 ele">
          {/* Money Melange */}
-         <a href='https://www.instagram.com/p/C2opSv9yFFC/' target='_blank' rel='noreferrer'>
+         <div class="image-wrapper">
        <img src={Melange} alt="Money Melange"/>
+       <div class="overlay">
+         <a href='https://www.instagram.com/p/C2opSv9yFFC/' target='_blank' rel='noreferrer'>
+       <i className="fab fa-instagram"/>
        </a>
+           </div>
+       </div>
        </div>
 
        <h2 className="title">Money Melange</h2>
@@ -63,12 +98,17 @@ const Events = () => {
       
        
        <div className="element">
-       <div className="element1">
+       <div className="element1 ele">
          {/* Kharche pe Charcha */}
-         <a href='https://www.instagram.com/p/C2AVhH4SE0P/' target='_blank' rel='noreferrer'>
+         <div class="image-wrapper">
          <img src={KharchePeCharche} alt="kharche pr charcha"/>
+         <div class="overlay">
+         <a href='https://www.instagram.com/p/C2AVhH4SE0P/' target='_blank' rel='noreferrer'>
+         <i className="fab fa-instagram"/>
          </a>
        </div>
+           </div>
+         </div>
       
        <h2 className="title">Kharche Pe Charche</h2>
        </div>
@@ -91,11 +131,16 @@ const Events = () => {
       
 
          <div className="element">
-       <div className="element1">
+       <div className="element1 ele">
          {/* bidding bitz */}
-         <a href='https://www.instagram.com/p/CzqYKwRLp24/' target='_blank' rel='noreferrer'>
+         <div class="image-wrapper">
          <img src={BiddingBlitz} alt="speaker"/>
+         <div class="overlay">
+         <a href='https://www.instagram.com/p/CzqYKwRLp24/' target='_blank' rel='noreferrer'>
+         <i className="fab fa-instagram"/>
          </a>
+           </div>
+         </div>
        </div>
       
        <h2 className="title">Bidding Blitz</h2>
@@ -104,11 +149,16 @@ const Events = () => {
 
 
        <div className="element">
-       <div className="element2">
+       <div className="element2 ele">
          {/* speaker */}
-         <a href='https://www.instagram.com/p/CzbC7cLPrex/' target='_blank' rel='noreferrer'>
+         <div class="image-wrapper">
        <img src={Speaker} alt="speaker"/>
+       <div class="overlay">
+         <a href='https://www.instagram.com/p/CzbC7cLPrex/' target='_blank' rel='noreferrer'>
+       <i className="fab fa-instagram"/>
        </a>
+           </div>
+       </div>
        </div>
 
        <h2 className="title">Guide To Stock Market</h2>
@@ -124,11 +174,16 @@ const Events = () => {
 
        <div className="element">
          
-       <div className="element3">
+       <div className="element3 ele">
          {/*  college trader showdown*/}
-         <a href='https://www.instagram.com/p/CxXRs1Pywdn/' target='_blank' rel='noreferrer'>
+         <div class="image-wrapper">
          <img src={CollegeTrading} alt="speaker"/>
+         <div class="overlay">
+         <a href='https://www.instagram.com/p/CxXRs1Pywdn/' target='_blank' rel='noreferrer'>
+         <i className="fab fa-instagram"/>
          </a>
+           </div>
+         </div>
        </div>
 
        <h2 className="title">College Trader Showdown</h2>
@@ -139,11 +194,16 @@ const Events = () => {
 
        <div className="element">
          
-       <div className="element4">
+       <div className="element4 ele">
          {/*  orientation*/}
-         <a href='https://www.instagram.com/p/CxAA9weLEea/?img_index=1' target='_blank' rel='noreferrer'>
+         <div class="image-wrapper">
          <img src={Orientation} alt="speaker"/>
+         <div class="overlay">
+         <a href='https://www.instagram.com/p/CxAA9weLEea/?img_index=1' target='_blank' rel='noreferrer'>
+         <i className="fab fa-instagram"/>
          </a>
+           </div>
+         </div>
        </div>
 
        <h2 className="title">Orientation</h2>
