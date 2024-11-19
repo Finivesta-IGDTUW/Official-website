7c22c56c3e9cb68737d00e870638fd9e89d212e9
urvashi2004 <uy2204@gmail.com>
2024-11-19 16:14:33 +0530

    Added games section

--- src/NavbarFooter/Navbar.js
+++ src/NavbarFooter/Navbar.js
@@ -63,6 +63,7 @@ const Navbar = () => {
                   <li><a href="/resources/trading">Trading Applications</a></li>
               </ul>
           </li>
+          <li><a href="/games">Games</a></li>
           <li><a href="/team">Team</a></li>
           <li><a href="/contact">Contact Us</a></li>
         </ul>
