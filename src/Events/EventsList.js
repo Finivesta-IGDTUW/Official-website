e05112de659aa22ee684ec21f8c671be4ddc4fb2
Mahi Arora <aroramahi5432@gmail.com>
2025-01-27 21:08:28 +0530

    Update Bylines

--- src/Events/EventsList.js
+++ src/Events/EventsList.js
@@ -22,7 +22,7 @@ const events = [
     name: 'Unveiling IPOs',
     photo: Unveiling_IPO,
     link: 'https://www.canva.com/design/DAGbu4uhDfU/AVRFjLBax5A2e-bE1gvttw/view?utm_content=DAGbu4uhDfU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb1e6d44f9b',
-    byline: 'Discover the secrets of IPOs',
+    byline: 'Master the basics of IPO and get a headstart into the world of finance.',
     year: 2025,
     backgroundColor: '#439543'
   },
@@ -30,7 +30,7 @@ const events = [
     name: 'Money Masterclass',
     photo: MoneyMasterclass,
     link: '/resources/moneymasterclass',
-    byline: 'Learn payroll systems',
+    byline: ' Our weekly initiative to promote financial literacy among our members and build expertise in both personal and professional finance',
     year: 2024,
     backgroundColor: '#079b6e'
   },
@@ -38,7 +38,7 @@ const events = [
     name: 'Finlatics',
     photo: Finlatics,
     link: 'https://www.linkedin.com/feed/update/urn:li:activity:7160561756463853568/',
-    byline: 'Explore financial literacy with Finlatics',
+    byline: 'Online webinar on Stock Trading and Investment Banking that unveils the secrets to success in the ever-evolving financial landscape.',
     year: 2024,
     backgroundColor: '#fea106'
   },
@@ -46,7 +46,7 @@ const events = [
     name: 'Money Melange',
     photo: Melange,
     link: 'https://www.instagram.com/p/C2CkzBTStUb/?igsh=MW5lNmZ6NmRxZTdqMA==',
-    byline: 'A blend of money and fun',
+    byline: ' A challenge to your strategic and creative prowess, solve financially charged problems in a totally imaginative way.',
     year: 2024,
     backgroundColor: '#c5f7c5'
   },
@@ -54,7 +54,7 @@ const events = [
     name: 'Kharche Pe Charcha',
     photo: KharchePeCharche,
     link: 'https://www.instagram.com/p/C2AVhH4SE0P/',
-    byline: 'Discussing expenses',
+    byline: 'An engaging conversation on money management techniques and to learn the fundamentals of budgeting and develop your financial literacy.',
     year: 2024,
     backgroundColor: '#156c53'
   },
@@ -62,7 +62,7 @@ const events = [
     name: 'Bidding Blitz',
     photo: BiddingBlitz,
     link: 'https://www.instagram.com/p/CzqYKwRLp24/',
-    byline: 'Exciting bidding competition',
+    byline: 'Where every bid is a move and every move a chance to victory.',
     year: 2023,
     backgroundColor: '#70b236'
   },
@@ -70,7 +70,7 @@ const events = [
     name: 'Guide To Stock Market',
     photo: Speaker,
     link: 'https://www.instagram.com/p/CzbC7cLPrex/',
-    byline: 'Learn about the stock market',
+    byline: 'An insightful webinar from a speaker with 20+ years’ experience in marketing and sales.',
     year: 2023,
     backgroundColor: '#1f433d'
   },
@@ -78,7 +78,7 @@ const events = [
     name: 'College Trader Showdown',
     photo: CollegeTrading,
     link: 'https://www.instagram.com/p/CxXRs1Pywdn/',
-    byline: 'College trading competition',
+    byline: 'A NATIONAL LEVEL VIRTUAL TRADING CONTEST, with virtual currency and real money to your bank accounts.',
     year: 2023,
     backgroundColor: '#3aed36'
   },
@@ -86,7 +86,7 @@ const events = [
     name: 'Orientation',
     photo: Orientation,
     link: 'https://www.instagram.com/p/CxAA9weLEea/?img_index=1',
-    byline: 'Welcome to the new members',
+    byline: 'The finance society of IGDTUW that aims to bring financial knowledge to the girls of our college.',
     year: 2023,
     backgroundColor: '#6eb335'
   },
