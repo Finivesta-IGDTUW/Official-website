f65708d1ba0505a1ec0906fee2d52206d1815fa0
urvashi2004 <uy2204@gmail.com>
2025-02-05 15:34:00 +0530

    Final changes

--- src/Events/FinWeek25/FinWeek25.js
+++ src/Events/FinWeek25/FinWeek25.js
@@ -23,7 +23,7 @@ const FinWeek25 = () => {
           </a>
         </div>
       </div>
-      <h2 className="timeline-title">Events This Week</h2>
+      <h2 className="timeline-title">Timeline</h2>
       <div className="timeline-landmark">
         <div className="ludo-piece">
           <div className="oval-base"></div>
@@ -36,11 +36,11 @@ const FinWeek25 = () => {
       <div className="timeline">
         <div className="timeline-line-full"></div>
         
-        <div className="timeline-box-container">
+        <div className="timeline-box-container cont-left finweek-first">
           <div className="timeline-box left left-line">
             <div className="timeline-content" data-aos="fade-right">
               <h3 className="event-name">Speaker Session 01</h3>
-              <button className="event-button" onClick={() => window.location.href = '#'}>Register Here</button>
+              <button className="event-button" onClick={() => window.location.href = '#'}>Registrations Open Soon</button>
             </div>
             <div className="circle"></div>
           </div>
@@ -51,13 +51,12 @@ const FinWeek25 = () => {
                 <p className="event-day">Monday</p>
                 <p className="event-venue"><b>Venue: </b> TBA</p>
               <p className="event-participation"><b>Participation: </b> Only IGDTUW</p>
-                <a href="#" className="event-link" target="_blank" rel="noreferrer">Know More</a>
               </div>
               </div>
           </div>
         </div>
 
-        <div className="timeline-box-container">
+        <div className="timeline-box-container cont-right">
           <div className="timeline-box left">
             <div className="timeline-content" data-aos="fade-right">
               <div className="event-date-day">
@@ -65,20 +64,19 @@ const FinWeek25 = () => {
                 <p className="event-day">Tuesday</p>
                 <p className="event-venue"><b>Venue: </b> Seminar Hall</p>
                 <p className="event-participation"><b>Participation: </b> Only IGDTUW</p>
-                <a href="https://www.instagram.com/p/DFcP10GT9qe/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className="event-link" target="_blank" rel="noreferrer">Know More</a>
               </div>
             </div>
           </div>
           <div className="timeline-box right right-line">
             <div className="timeline-content" data-aos="fade-left">
             <h3 className="event-name">Finopoly</h3>
-            <button className="event-button" onClick={() => window.location.href = '#'}>Register Here</button>
+            <button className="event-button" onClick={() => window.location.href = '#'}>Registrations Open Soon</button>
           </div>
           <div className="circle"></div>
           </div>
         </div>
 
-        <div className="timeline-box-container">
+        <div className="timeline-box-container cont-left">
           <div className="timeline-box left left-line">
             <div className="timeline-content" data-aos="fade-right">
             <h3 className="event-name">Game Night</h3>
@@ -94,14 +92,13 @@ const FinWeek25 = () => {
                 <p className="event-day">Wednesday</p>
                 <p className="event-venue"><b>Venue: </b> Online</p>
                 <p className="event-participation"><b>Participation: </b> Open For All</p>
-                <a href="#" className="event-link" target="_blank" rel="noreferrer">Know More</a>
               </div>
               
             </div>
           </div>
         </div>
 
-        <div className="timeline-box-container">
+        <div className="timeline-box-container cont-right">
           <div className="timeline-box left">
             <div className="timeline-content" data-aos="fade-right">
             <div className="event-date-day">
@@ -109,22 +106,19 @@ const FinWeek25 = () => {
                 <p className="event-day">Thursday</p>
                 <p className="event-venue"><b>Venue: </b> Online</p>
                 <p className="event-participation"><b>Participation: </b> Open For All</p>
-                <a href="#" className="event-link" target="_blank" rel="noreferrer">Know More</a>
               </div>
             </div>
           </div>
           <div className="timeline-box right right-line">
             <div className="timeline-content" data-aos="fade-left">
             <h3 className="event-name">Speaker Session 02</h3>
-              <button className="event-button" onClick={() => window.location.href = '#'}>Register Here</button>
-              
+              <button className="event-button" onClick={() => window.location.href = '#'}>Registrations Open Soon</button>
             </div>
-            
           <div className="circle"></div>
           </div>
         </div>
 
-        <div className="timeline-box-container">
+        <div className="timeline-box-container cont-left finweek-last">
           <div className="timeline-box left left-line last-box">
             <div className="timeline-content" data-aos="fade-right">
             <h3 className="event-name">Pitch Desk : B Plan</h3>
@@ -141,7 +135,6 @@ const FinWeek25 = () => {
                 <p className="event-venue"><b>Venue: </b> IIIT-D</p>
                 
               <p className="event-participation"><b>Participation: </b> Open For All</p>
-                <a href="#" className="event-link">Know More</a>
               </div>
               
             </div>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,8 +6,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-import F1 from "./Images/F1.jpg";
-
 const Home = () => {
   var settings = {
     dots: true,
