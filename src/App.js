47a24b34cbf3d2bc5ae5cad997a3a2283513fc11
RuzalSingla <ruzalsingla2301@gmail.com>
2025-02-14 21:19:52 +0530

    Budgeting Game

--- src/App.js
+++ src/App.js
@@ -55,6 +55,17 @@ import Review13 from "./Resources/BookReviews/bookpages/Review13";
 
 import FinWeek25 from './Events/FinWeek25/FinWeek25.js';
 
+import Games from './Games/Game';
+import One from './Games/BudgetingGame/one.js';
+import Two from './Games/BudgetingGame/two.js';
+import Three from './Games/BudgetingGame/three.js';
+import Four from'./Games/BudgetingGame/four.js';
+import Five from './Games/BudgetingGame/five.js';
+import Six from './Games/BudgetingGame/six.js';
+import Seven from './Games/BudgetingGame/seven.js';
+import Eight from './Games/BudgetingGame/eight.js';
+import Nine from './Games/BudgetingGame/nine.js';
+
 
 const AppContent = () => {
 
@@ -131,6 +142,18 @@ const AppContent = () => {
             <Route path="/resources/videos" element={<Youtube/>}/>
             <Route path="/resources/trading" element={<TradingApps/>}/>
 
+            <Route path="/games" element={<Games/>}/>
+          <Route path="/games/budgetinggame" element={<One/>}/>
+          <Route path="/games/Budgetingame/two.js" element={<Two/>}/>
+          <Route path="/games/Budgetingame/three.js" element={<Three/>}/>
+          <Route path="/games/Budgetinggame/four.js" element={<Four/>}/>
+          <Route path="/games/Budgetinggame/five.js" element={<Five/>}/>
+          <Route path="/games/Budgetinggame/six.js" element={<Six/>}/>
+          <Route path="/games/Budgetinggame/seven.js" element={<Seven/>}/>
+          <Route path="/games/Budgetinggame/eight.js" element={<Eight/>}/>
+          <Route path="/games/Budgetinggame/nine.js" element={<Nine/>}/>
+          
+          
           <Route path="/team" element={<Team/>}/>
           <Route path="/contact" element={<Contact/>}/>
         </Routes>
